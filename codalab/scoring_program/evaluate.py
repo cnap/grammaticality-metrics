abf272b323340eb1610e9ebb8027ae6f05bd6744
Courtney Napoles <cdnapoles@gmail.com>
2016-11-15 14:41:46 -0500

    LT 3.1

--- codalab/scoring_program/evaluate.py
+++ codalab/scoring_program/evaluate.py
@@ -19,12 +19,15 @@ from gleu import GLEU
 from m2scorer.m2scorer import load_annotation as load_m2_annotation
 from imeasure.ieval import IMeasure
 
-print('PATH:', sys.path)
-cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
-print('SCRIPT DIR:', cwd)
-print('CONTENTS SCRIPT DIR', os.listdir(cwd))
+skip_im = True
+debug = False
 
-skip = True
+cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
+if debug:
+    sys.stderr.write('PATH: %s\n' % sys.path)
+    sys.stderr.write('SCRIPT DIR: %s\n' % cwd)
+    sys.stderr.write('CONTENTS SCRIPT DIR: %s\n' % os.listdir(cwd))
+
 # GLOBAL VARIABLES
 REF_NAMES = ['NUCLE', 'EXPFLUENCY', 'EXPMIN', 'TURKMIN', 'TURKFLUENCY']
 ## lambdas are stored in the order: lambda_rho, lambda_r
@@ -68,9 +71,9 @@ def compute_im(reference, prediction_path, skip=False):
     im.file_hyp = prediction_path
     im.file_ref = reference
     im.per_sent = True
-    # im.verbose, im.v_verbose = True, True
+    if debug:
+        im.verbose, im.v_verbose = True, True
     im_result = [i for i in im.run()]
-    #print im_result
     return np.array(im_result)
 
 
@@ -79,42 +82,45 @@ def call_lt(_sentences):
     sys.stderr.write('Running LanguageTool...\n')
     detokenizer = Detokenizer()
     sentences = [detokenizer.detokenize(sent) for sent in _sentences]
-    print('Java info:', os.system('which java'), os.system('java -version'))
+    if debug:
+        sys.stderr.write('Java info: %s %s\n' % (os.system('which java'), os.system('java -version')))
     process = Popen(['java', '-Dfile.encoding=utf-8',
-                     '-jar', os.path.join(cwd, 'LanguageTool-3.1/languagetool-commandline.jar'), '-d',
-                     'COMMA_PARENTHESIS_WHITESPACE,WHITESPACE_RULE,' +
+                     '-jar', os.path.join(cwd, 'LanguageTool-3.1/languagetool-commandline.jar'),
+                     '-d', 'COMMA_PARENTHESIS_WHITESPACE,WHITESPACE_RULE,' +
                      'EN_UNPAIRED_BRACKETS,EN_QUOTES',
-                     '--line-by-line', '-l', 'en-US', '-c', 'utf-8'],
+                     '-b', '-l', 'en-US', '-c', 'utf-8'],
                     stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    ret = process.communicate(input=('\n\n'.join(sentences)).encode('utf-8'))
-    print('LT out:', ret)
+    ret = process.communicate(input=('\n'.join(sentences)).encode('utf-8'))
+    if debug:
+        sys.stderr.write('LT out: %s\n' % str(ret))
     counts = [0] * len(sentences)
     for l in ret[0].split('\n'):
         if 'Rule ID' in l:
             ll = l.split()
-            ind = (int(ll[2][:-1]) - 1) / 2
+            ind = (int(ll[2][:-1]) - 1)
             counts[ind] += 1
     return np.array(counts)
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        sys.stderr.write('Usage: python evaluate.py input-dir output-dir [--skip_im] [--ref:NUCLE|EXPFLUENT|EXPMIN|TURKFLUENT|TURKMIN]\n')
-        sys.exit(1)
+        sys.stderr.write('Usage: python evaluate.py input-dir output-dir [--im] [--ref:NUCLE|EXPFLUENT|EXPMIN|TURKFLUENT|TURKMIN] [-d]\n')
+        sys.exit(0)
 
     # setup paths and load predictions
     input_dir = sys.argv[1]
     output_dir = sys.argv[2]
 
-    skip_im = False
     use_refs = REF_NAMES
 
     if len(sys.argv) > 3:
         for arg in sys.argv[3:]:
-            if arg == '--skip_im':
-                skip_im = True
+            if arg == '--im':
+                skip_im = False
             elif arg.startswith('--ref'):
                 use_refs = [arg.split(':')[1]]
+            elif arg == '-d':
+                debug = True
 
     prediction_path = os.path.join(input_dir, 'res/answer.txt')
     reference_dir = os.path.join(input_dir, 'ref')
@@ -128,11 +134,12 @@ if __name__ == '__main__':
     # call metrics and colect scores
     scores = []
     lt_score = call_lt(predictions)
-    print('Reference dir:', [o for o in os.walk(reference_dir)])
+    if debug:
+        sys.stderr.write('Reference dir: %s\n' % str([o for o in os.walk(reference_dir)]))
 
     for ref in use_refs:
         im = compute_im(os.path.join(reference_dir, ref + '.m2.ieval.xml'),
-                        prediction_path, skip=skip)
+                        prediction_path, skip=skip_im)
         if im is not None:
             scores.append(('I-measure', ref, im))
         gleu = compute_gleu(os.path.join(reference_dir, 'source'),
